a6d194dca9a68f32506c29c2d760c217e709a65e
AOvsyannikov <aovsyannikov@it-one.ru>
2024-05-01 17:22:35 +0300

    added FullRegistration.java

---
+++ src/test/java/tests/FullRegistration.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FullRegistration {
+}
